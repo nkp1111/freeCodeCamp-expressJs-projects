9e328dd5351e4bea3d585bf793c64563e97257e6
neeraj <neerajp4321@gmail.com>
2022-11-06 18:49:59 +0530

    Feat: add url and get whether info

--- 1-show-local-whether/index.js
+++ 1-show-local-whether/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
-
+const axios = require('axios')
 const app = express()
+
+const url = 'https://weather-proxy.freecodecamp.rocks/api/current'
+
+const getWeather = (url, lat, lon) => {
+  let latLong = `?lat=${lat}&lon=${lon}`
+  axios.get(url + latLong)
+    .then(data => console.log(data.data))
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+getWeather(url, 35, 139)
 
 app.listen('3000', () => {
   console.log('App on port 3000');
