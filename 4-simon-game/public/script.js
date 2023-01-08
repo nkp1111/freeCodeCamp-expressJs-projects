837a0768556a1cce3ade0616bdbf3745d47acbae
nkp1111 <neerajp4321@gmail.com>
2023-01-08 11:08:54 +0530

    Feat: add game function not working write

--- 4-simon-game/public/script.js
+++ 4-simon-game/public/script.js
@@ -3,56 +3,107 @@ const displayEl = document.querySelector(".display")
 const startBtn = document.querySelector(".radio-switch")
 const startLabel = document.querySelector(".radio-switch-label")
 
-let score = 0
+let score
 let initialWaitTime = 1000
 
-let gameChoiceList = ["red", "green", "blue", "yellow"]
+// add items on list as game goes on
+let gameChoiceList = []
+// user response refreshes after each turn
+let userResponse = []
+// game boxes listen click event
 let boxes = document.querySelectorAll(".color-box")
+// available choices
 let choices = ["red", "green", "blue", "yellow"]
 
-showDisplay()
+let isGameOn = false
+let isGameStart = false
+let isGameOver = false
 
-// make the game on and off 
+// ----------------------------------------------------------- //
+// GAME ON and OFF
 OnOffBtn.addEventListener("click", (e) => {
   if (e.target.classList.contains("on")) {
     e.target.classList.remove("on")
     e.target.classList.add("off")
-    gameChoiceList = []
+    gameOff()
   } else {
     e.target.classList.add("on")
     e.target.classList.remove("off")
-    gameStart()
+    gameOn()
   }
 })
 
-function gameStart() {
+function gameOff() {
+  gameChoiceList = []
+  userResponse = []
+  isGameOn = false
+  gameStop()
+}
+
+function gameOn() {
   // after the game is on it started the game
+  if (!score) {
+    score = 0
+  }
+  isGameOn = true
   showDisplay()
   startBtn.addEventListener("click", updateStart)
   startLabel.addEventListener("click", updateStart)
 }
+// ----------------------------------------------------------- //
+// ############ //
+// ----------------------------------------------------------- //
+// game START and STOP
+
+function updateStart() {
+  // start button start and stop the current game
+  if (startLabel.innerText == "START") {
+    startBtn.style.background = "green"
+    isGameStart = true
+    startLabel.innerText = "STOP"
+    console.log(isGameOn, isGameOver, isGameStart);
+    if (!isGameOver && isGameStart) {
+      playGame()
+    }
+  }
+  else {
+    startBtn.style.background = "red"
+    isGameStart = false
+    startLabel.innerText = "START"
+  }
+}
+
+function gameStop() {
+  // stop the game 
+  startLabel.innerText = "START"
+  startBtn.style.background = "red"
+  score = null
+  isGameStart = false
+  startBtn.removeEventListener("click", updateStart)
+  startLabel.removeEventListener("click", updateStart)
+  showDisplay()
+}
+// ----------------------------------------------------------- //
+// ############ //
+// ----------------------------------------------------------- //
+// display SCORE
 
 function showDisplay() {
   // after score change update score
   displayEl.innerText = score
 }
-
-function updateStart() {
-  // start button start and stop the current game
-  if (startLabel.innerText == "START") {
-    startLabel.innerText = "STOP"
-    startBtn.style.background = "green"
-    playGame()
-  } else {
-    startLabel.innerText = "START"
-    startBtn.style.background = "red"
-  }
-}
+// ----------------------------------------------------------- //
+// ############ //
+// ----------------------------------------------------------- //
+// Start the GAME
 
 function playGame() {
   // start the game play show colors
   getRandomColor()
-  showColors(0)
+  console.log(gameChoiceList)
+  if (!showColors(0)) {
+    hearResponse()
+  }
 }
 
 function getRandomColor() {
@@ -63,23 +114,70 @@ function getRandomColor() {
 
 function showColors(current) {
   // Brighten the current colors
+  if (current === gameChoiceList.length) {
+    return false
+  }
+
   for (let box of boxes) {
     if (box.classList.contains(`${gameChoiceList[current]}-box`)) {
       box.classList.add("select")
     }
-
-    setTimeout(function () {
-      box.classList.remove("select")
-      if (current < gameChoiceList.length - 2) {
-        showColors(current + 1)
-      } else {
-        hearResponse()
+    setTimeout(() => {
+      removeSelectFromBoxes()
+      if (!showColors(current + 1)) {
+        return false
       }
     }, initialWaitTime)
   }
 }
 
+function removeSelectFromBoxes() {
+  // remove select class from earlier box
+  boxes.forEach(box => {
+    if (box.classList.contains("select")) {
+      box.classList.remove("select")
+    }
+  })
+}
 
 function hearResponse() {
   // hear user response 
+  boxes.forEach(box => {
+    box.addEventListener("click", (e) => {
+      removeSelectFromBoxes()
+      userResponse.push(box.classList[1].split("-")[0])
+      box.classList.add("select")
+      console.log(userResponse);
+      if (userResponse.length === gameChoiceList.length) {
+        checkResponse()
+      }
+    })
+  })
+
+}
+
+function checkResponse() {
+  // check if current answer is right
+  console.log(gameChoiceList, userResponse, score)
+  let fail = false
+  userResponse.forEach((res, ind) => {
+    if (res !== gameChoiceList[ind]) {
+      fail = true
+    }
+  })
+
+  if (fail) {
+    score = 0
+    userResponse = []
+    gameChoiceList = []
+    showDisplay()
+    updateStart()
+  } else {
+    score += 1
+    userResponse = []
+    showDisplay()
+    if (!isGameOver && isGameStart) {
+      playGame()
+    }
+  }
 }
