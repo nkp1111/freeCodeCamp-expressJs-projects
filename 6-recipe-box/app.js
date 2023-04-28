ee3c0a028f43274eda35f5ab03a371e99d9703fc
nkp1111 <neerajp4321@gmail.com>
2023-04-28 11:41:04 +0530

    feat: add route for viewing recipes

--- 6-recipe-box/app.js
+++ 6-recipe-box/app.js
@@ -5,10 +5,18 @@ const recipes = require("./recipes")
 
 app.set("view engine", "ejs")
 app.set("views", __dirname + "/views")
+app.use(express.static(__dirname + "/public"))
 
+let currentRecipe = 0
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { recipes })
+  res.render("index.ejs", { recipes, currentRecipe })
+})
+
+app.get("/:recipeInd", (req, res) => {
+  const { recipeInd } = req.params
+  currentRecipe = recipeInd
+  res.redirect("/")
 })
 
 app.listen(port, () => {
